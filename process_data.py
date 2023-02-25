c92120e2077d3acc73ba07a745bca4b342c5686b
Andy Gnias <AGnias47@gmail.com>
2023-02-25 14:28:53 -0500

    Persistent datset

--- process_data.py
+++ process_data.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Resources
+---------
+https://github.com/LucasFidon/TRABIT_BraTS2021/blob/main/src/data/brats21_dataset.py
+"""
+
+from monai.data import PersistentDataset
+
 from data.process_data import dataset_dicts
 from data.data_transforms import transform_function
 
@@ -6,3 +16,4 @@ dataset = dataset_dicts()
 data_transform_function = transform_function()
 data_dict_transform = data_transform_function(dataset[0])
 print(data_dict_transform["flair"].shape, data_dict_transform["seg"].shape)
+persistent_dataset = PersistentDataset(dataset, data_transform_function, "local_data/persistent_dataset/train")
